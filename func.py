10d020257ae1d5b36599e7392fe4d70f65458e16
Ken Taylor <kentaylorappdev@gmail.com>
2022-05-15 01:28:10 -0400

    restructured the youtube stuff, moved to the class

--- func.py
+++ func.py
@@ -1,8 +1,6 @@
 import os
 import sys
-import yt_downloader
 from sys import platform
-import subprocess
 from pathlib import Path
 
 
@@ -31,68 +29,10 @@ def get_os():
         print('err')
 
 
-# class BoxLayoutUI(BoxLayout):
-#     link = ObjectProperty(None)
-#     op_input = ObjectProperty(None)
-#     update_label = ObjectProperty(None)
-#     download_button = ObjectProperty(None)
-#     main_canv_image = resource_path('bg.png')
-#     yt_logo = resource_path('ytlogo.png')
-
-    def download_button_action(self):
-        self.download_button.disabled = True
-        # os_download_location = get_os()
-        # print(desktop)
-        if self.link.text == '':
-            self.update_label.text = 'ERROR - Please enter a song name and artiste'
-            return
-        self.update_label.text = 'Searching...'
-
-        default_loc = get_os() + '/Youtube/'  # Default folder
-        if self.op_input.text == '':
-            download_info = yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text), default_loc)
-        else:
-            user_location = default_loc + self.op_input.text
-            download_info = yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text),
-                                                                  user_location)
-
-        self.update_label.text, file_path, song_info = download_info
-        # print(song_info)
-        try:
-            rename_file(file_path)  # remove the word downloaded 11 characters, its the title so i add mp4
-        except Exception as e:
-            print(str(e))
-
-        self.link.text = ''
-        self.download_button.disabled = False
-
-    def open_folder(self):
-        # desktop = os.path.expanduser("~\\Desktop\\")
-        # print(desktop)
-        default_loc = get_os() + '/Youtube/'
-        # print(default_loc)
-        if self.op_input.text == '':
-            path = default_loc
-        else:
-            path = default_loc + self.op_input.text
-
-        if platform == "windows":
-            try:
-                os.startfile(path)
-            except Exception as e:
-                print(e)
-
-        elif platform == "darwin":
-            try:
-                subprocess.Popen(["open", path])
-            except Exception as e:
-                print(e)
-        else:
-            try:
-                print(platform)
-                subprocess.Popen(["xdg-open", path])
-            except Exception as e:
-                print(e)
+def get_songs_from_text(txt):
+    with open(txt) as fp:
+        Lines = fp.readlines()
+        return Lines
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,15 +1,17 @@
+from pytube import YouTube
+from pytube import Search
+from pytube import Playlist
 import sys
 from PyQt5 import uic
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QStackedWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QRadioButton
+from PyQt5.QtWidgets import QApplication, QStackedWidget, QMainWindow, QPushButton, QLabel, QLineEdit, QRadioButton
 import os
-import yt_downloader
 from sys import platform
 import subprocess
-from pathlib import Path
 import func
 import ssl
+
 ssl._create_default_https_context = ssl._create_unverified_context  # important used to make internet coms legit - windows issue
+
 
 class MainUiWindow(QMainWindow):
     def __init__(self):
@@ -27,32 +29,151 @@ class MainUiWindow(QMainWindow):
 
         # Actions
         self.download_button.clicked.connect(lambda: self.download_clicked())
-        # self.open_folder.clicked.connect(lambda: self.action())
+        self.open_folder.clicked.connect(lambda: self.open_folder_clicked())
+        self.download_list_button.clicked.connect(lambda: self.open_folder_clicked())
 
     def download_clicked(self):
+        radio_button_state = "radio edit clean audio"
         if self.link.text() == '':
             self.update_label.setText("ERROR - Please enter a song name and artiste")
             return
+        if self.select_audio.isChecked():
+            radio_button_state = "official audio"
+        elif self.select_raw_audio.isChecked():
+            radio_button_state = "raw official audio"
+        elif self.select_clean_audio.isChecked():
+            radio_button_state = "radio edit clean audio"
+
         self.update_label.setText('Searching...')
         default_loc = func.get_os() + '/Youtube/'  # Default folder
         if self.op_input.text() == '':
-            download_info = yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text()), default_loc)
+            download_info = self.youtube_single_download(
+                self.searchtube(self.link.text(), radio_button_state), default_loc)
         else:
             user_location = default_loc + self.op_input.text()
-            download_info = yt_downloader.youtube_single_download(yt_downloader.searchtube(self.link.text()),
-                                                                  user_location)
+            download_info = self.youtube_single_download(
+                self.searchtube(self.link.text(), radio_button_state),
+                user_location)
         self.update_label.setText(download_info[0])
         file_path = download_info[1]
         song_info = download_info[2]
+        try:
+            func.rename_file(file_path)  # remove the word downloaded 11 characters, its the title so i add mp4
+        except Exception as e:
+            print(str(e))
 
+        self.link.setText("")
+        self.download_button.disabled = False
 
+    def open_folder_clicked(self):
+        # desktop = os.path.expanduser("~\\Desktop\\")
+        # print(desktop)
+        default_loc = func.get_os() + '/Youtube/'
+        print(default_loc)
+        if self.op_input.text() == '':
+            path = default_loc
+        else:
+            path = default_loc + self.op_input.text()
 
-        # if self.select_audio.isChecked():
-        #     self.update_label.setText("just audio")
-        # elif self.select_raw_audio.isChecked():
-        #     self.update_label.setText("raw audio")
-        # elif self.select_clean_audio.isChecked():
-        #     self.update_label.setText("clean audio")
+        if platform == "win32":
+            try:
+                os.startfile(path)
+            except Exception as e:
+                print(e)
+
+        elif platform == "darwin":
+            try:
+                subprocess.Popen(["open", path])
+            except Exception as e:
+                print(e)
+        else:
+            try:
+                print(platform)
+                subprocess.Popen(["xdg-open", path])
+            except Exception as e:
+                print(e)
+
+    def download_list_clicked(self):
+        list_of_songs = func.get_songs_from_text('listofsongs.txt')
+        radio_button_state = "radio edit clean audio"
+        if self.select_audio.isChecked():
+            radio_button_state = "official audio"
+        elif self.select_raw_audio.isChecked():
+            radio_button_state = "raw official audio"
+        elif self.select_clean_audio.isChecked():
+            radio_button_state = "radio edit clean audio"
+
+        self.update_label.setText('Searching...')
+        default_loc = func.get_os() + '/Youtube/'  # Default folder
+        if self.op_input.text() == '':
+            download_info = self.youtube_single_download(
+                self.searchtube(self.link.text(), radio_button_state), default_loc)
+        else:
+            user_location = default_loc + self.op_input.text()
+            download_info = self.youtube_single_download(
+                self.searchtube(self.link.text(), radio_button_state),
+                user_location)
+        self.update_label.setText(download_info[0])
+        file_path = download_info[1]
+        song_info = download_info[2]
+        try:
+            func.rename_file(file_path)  # remove the word downloaded 11 characters, its the title so i add mp4
+        except Exception as e:
+            print(str(e))
+
+        self.link.setText("")
+
+        # *************************** YOUTUBE STUFF *************************
+
+    def youtube_single_download(self, link, op):
+        if link == []:
+            # print('Error - no song specified or song downloaded already')
+            return
+        # print('single download func ran')
+        yt = YouTube(link[
+                         0])  # not sure whay i does remove this indexing thing but i suspect its for thwee playlisting to work... after 2 weeks im lost when the program gcrashes lol i need to handle this better asap
+        # print(f'single download func debug 2 {yt}')
+        yt.streams.filter(only_audio=True)
+        # print("Starting download....")
+        stream = yt.streams.get_by_itag(140)
+        file_path = stream.download(output_path=op)
+        info_list = [yt.title, file_path, yt.vid_info]
+        return info_list
+
+    def searchtube(txt, radio_button_state):
+        if txt == '':
+            return []
+        # print('search func ran')
+        video_list = []
+        s = Search(f'{txt} {radio_button_state}')
+        for obj in s.results:
+            x = str(
+                obj)  # in the future see if theyy have an easier way to use these youtube obj in search results. I doubt what i'm doing is the easy way lol
+            video_id = x[x.rfind('=') + 1:].strip('>')
+            video_url = f'https://www.youtube.com/watch?v={video_id}'
+            video_list.append(video_url)
+        # print(f'search complete {video_list}')
+        return video_list
+
+    def download_youtube_playlist(self):
+        print('playlist func ran')
+        pl = input('Paste playlist here >')
+        playlist = Playlist(pl)
+        print('*' * 40)
+        print(f'Playlist contains {len(playlist)} items')
+        print('*' * 40)
+        for url in playlist[:3]:
+            self.youtube_single_download(url)
+
+    def download_list_of_songs_from_file(self, list_of_songs):
+        print('playlist from file func ran')
+        # playlist = Playlist(pl)
+        print('*' * 40)
+        print(f'Playlist from file contains {len(list_of_songs)} items')
+        print('*' * 40)
+        for song in list_of_songs:
+            print(song)
+            self.youtube_single_download(song)
 
 
 if __name__ == "__main__":
